40cdac42c4d5871ed7937590f1856ab11e974e18
Sammy Larbi <sam@codeodor.com>
2011-10-25 06:28:08 -0500

    updated gemfile and install shell scripts to match new relative location on my HD to install the gem/plugin from dev environment

--- plugin-test31/install_plupload_plugin.sh
+++ plugin-test31/install_plupload_plugin.sh
@@ -1,3 +1,4 @@
 rm -rf vendor/plugins/plupload-rails3/
-git clone ../plupload-rails3/ vendor/plugins/plupload-rails3/
+rm -rf tmp/plupload-rails3
+git clone ../../plupload-rails3/ vendor/plugins/plupload-rails3/
 rails runner vendor/plugins/plupload-rails3/install.rb
